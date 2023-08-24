30f1f8c364c813c89fab8f94ed293fed909def1d
hamza-choudhary <humzach700@gmail.com>
2023-08-24 14:06:06 +0500

    largest perimeter triangle

---
+++ 03_sorting/19_largest_perimeter_triangle/main.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution
+{
+public:
+  int largestPerimeter(vector<int> &nums)
+  {
+    //! to make traingle these conditions must satisfy
+    // https://www.youtube.com/watch?v=2t2G4yFp2Zk
+    // s1 + s2 > s3
+    // s2 + s3 > s1
+    // s1 + s3 > s2
+
+    // the question is we have to find these sides as maximum the sides will be
+    // the heighest the parameter will be (s1 + s2 + s2)
+
+    // sol
+    // we first sort the nums then last 3 nums will max then we can move left one by one to find our sides
+
+    // as the array will be sorted we can only check
+    // 1st condition as s3 will be largest after sort then other 2 condiotns will be satisfiesd
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = nums.size() - 1; i >= 2; i--)
+    {
+      if (nums[i - 2] + nums[i - 1] > nums[i])
+      {
+        return nums[i - 2] + nums[i - 1] + nums[i];
+      }
+    }
+
+    return 0;
+  }
+};
+
+int main()
+{
+  vector<int> arr{1,2,1,10};
+  vector<int> arr2{1, 2, 3};
+
+  Solution s = Solution();
+  cout << boolalpha << endl;
+
+  // s.merge(arr, m, arr2, n);
+  cout << s.largestPerimeter(arr) << endl;
+
+  // vector<int> vec = s.sortArrayByParityII(arr);
+  // for (auto i : vec)
+  //   cout << i << " ";
+
+  return 0;
+}
