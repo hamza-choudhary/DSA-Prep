f7c567729d27f7e7ca8d976d5eadabf5bb1b0ece
hamza-choudhary <humzach700@gmail.com>
2023-08-30 11:41:44 +0500

    done no smaller than current

---
+++ 03_sorting/27_numbers_smaller_than_current/main.cpp
@@ -0,0 +1,66 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> smallerNumbersThanCurrent(vector<int> &nums)
+  {
+    //! https://youtu.be/C5u_hvbq1qQ?t=605
+
+    vector<int> bucket(101);
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      bucket[nums[i]]++;
+    }
+
+    // accumulative sum
+    for (int i = 1; i < bucket.size(); i++)
+    {
+      bucket[i] = bucket[i] + bucket[i - 1];
+    }
+
+    //? reason why we using -1 in update ans
+    // for (int i=0; i<bucket.size(); i++)
+    // {
+    //   cout << i << " " << bucket[i] << endl;
+    // }
+
+    // update ans
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if (nums[i] == 0)
+      {
+        nums[i] == 0;
+      }
+      else
+      {
+        nums[i] = bucket[nums[i] - 1];
+      }
+    }
+
+    return nums;
+  }
+};
+
+int main()
+{
+  vector<int> arr{8, 1, 2, 2, 3};
+  vector<int> arr2{6, 5, 4, 8};
+
+  Solution s = Solution();
+  cout << boolalpha << endl;
+
+  // s.merge(arr, m, arr2, n);
+  // cout << s.relativeSortArray(arr) << endl;
+
+  vector<int> vec = s.smallerNumbersThanCurrent(arr);
+  for (auto &val : vec)
+  {
+    cout << val << " ";
+  }
+
+  return 0;
+}
