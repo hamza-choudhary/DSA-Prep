3009c90170bba190e28730a9bc66ac1404a65396
hamza-choudhary <humzach700@gmail.com>
2024-02-22 21:34:15 +0500

    special array of x element

--- 03_sorting/33_special_arra_x_element_grether_or_equal_x/main.cpp
+++ 03_sorting/33_special_arra_x_element_grether_or_equal_x/main.cpp
@@ -7,20 +7,13 @@ class Solution
 public:
   int specialArray(vector<int> &nums)
   {
-
-
-    //!NOTE: although we had to create bucket of 1000 elements but we can
-    // ! 1. Find max element and create bucket till max no.
-    // ! 2. create bucket of size of nums and if nums[i] is >= nums.size we simply increment last element.
-    // ! because we we will do commulative sum its gonna repeat same sum until it hit next non-zero value [1, 0, 0, 3] => [1, 1, 1, 4]
-    // ! we will do commulative sum in reverse order as it will tell us how many elements are greater than current index
-    
     int n = nums.size(); // size of array
     vector<int> bucket(n + 1);
 
     for (int &num : nums)
     {
-      if (num >= n) // if number in array is greater or equal to our length
+      // if number in array is greater or equal to our length
+      if (num >= n)
       {
         bucket[n]++; // increment last element of our bucket
       }
@@ -30,36 +23,31 @@ public:
       }
     }
 
-    
+    // reason of -1 instead of -2 becasue our bucket is of size n + 1
+    //  and we telling to iterate n + 1 - 2 = n - 1 or bucket.size -1
     for (int i = bucket.size() - 2; i >= 0; i--)
     {
       //* store cumulative sum
       bucket[i] = bucket[i] + bucket[i + 1];
     }
 
-    //* Now for every index in arr, the corresponding value represents the number of
-    //* elements that are greater than or equal to index in nums. (Concept of counting sort)
+    //* Now for every index in arr, the corresponding value represents the
+    // number of
+    //* elements that are greater than or equal to index in nums. (Concept
+    // of counting sort)
 
-    //* now we will check if for any index in arr, if we have arr[index] == index then that will be our answer.
+    //* now we will check if for any index in arr, if we have arr[index] ==
+    // index then that will be our answer.
 
-    for (int i=0; i<bucket.size(); i++)
+    for (int i = 0; i < bucket.size(); i++)
     {
       if (bucket[i] == i)
         return i;
     }
-    
+
     return -1;
   }
 };
-
-// int res = 0;
-// for(int i = counts.length-1; i > 0; i--) {
-//     res += counts[i];
-//     if(res == i)
-//         return i;
-// }
-
-// return -1;
 
 int main()
 {
