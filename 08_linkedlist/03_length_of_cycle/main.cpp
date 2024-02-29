9179f69c2dbf4ec9bf1cb3ee8c50ac391d88b0e7
hamza-choudhary <humzach700@gmail.com>
2024-02-29 18:09:26 +0500

    length of linked list cycle

---
+++ 08_linkedlist/03_length_of_cycle/main.cpp
@@ -0,0 +1,70 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+class ListNode
+{
+public:
+  int val;
+  ListNode *next;
+  ListNode()
+  {
+    this->next = nullptr;
+    this->val = 0;
+  }
+  // ListNode(int x) {
+  //   this->next = nullptr;
+  //   this->val = x;
+  // }
+  // ListNode(int x, ListNode *next) {
+  //   this->next = next;
+  //   this->val = x;
+  // }
+};
+
+class Solution
+{
+public:
+  int sizeOfCycle(ListNode *head)
+  {
+    ListNode *fast = head; // fast pointer
+    ListNode *slow = head; // slow pointer
+
+    int length = 0;
+
+    while (fast != nullptr || slow != nullptr)
+    {
+      fast = fast->next->next;
+      slow = slow->next;
+
+      if (fast == slow) // it means there is cycle
+      {
+        // now we have to move slow again until it hit again fast
+        do
+        {
+          slow = slow->next;
+          length++;
+        } while (slow != fast);
+
+        return length;
+      }
+    }
+
+    return 0;
+  }
+};
+
+int main()
+{
+  // vector<int> arr{1, 2, 3, 4, 5, 6};
+  // cout << func_name(arr) << endl;
+
+  // for(auto val: arr)
+  //{
+  // cout << val << " "
+  // }
+
+  return 0;
+}
