ff2613d1e5a1c77c765d8923b32762b5f8ec696b
hamza-choudhary <humzach700@gmail.com>
2024-03-06 16:12:05 +0500

    bfs traversal

---
+++ 10_trees/02_[bfs]_binary_tree_level_order_traversal/main.cpp
@@ -0,0 +1,80 @@
+#include "bits/stdc++.h"
+// https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//! https://youtu.be/9D-vP-jcc-Y?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&t=1633
+
+class Solution
+{
+public:
+  vector<vector<int>> levelOrder(TreeNode *root)
+  {
+    vector<vector<int>> results;
+
+    if (root == nullptr)
+      return results;
+
+    queue<TreeNode *> queue;
+    queue.push(root);
+
+    while (!queue.empty())
+    {
+      int levelSize = queue.size();
+      vector<int> currentLevel;
+      for (int i = 0; i < levelSize; i++)
+      {
+        TreeNode *currentNode = queue.front();
+        queue.pop();
+
+        currentLevel.push_back(currentNode->val);
+
+        if (currentNode->left != nullptr)
+        {
+          queue.push(currentNode->left);
+        }
+        if (currentNode->right != nullptr)
+        {
+          queue.push(currentNode->right);
+        }
+      }
+      results.push_back(currentLevel);
+    }
+
+    return results;
+  }
+};
+
+int main()
+{
+  // vector<int> arr{1, 2, 3, 4, 5, 6};
+  // cout << func_name(arr) << endl;
+
+  // for(auto val: arr)
+  //{
+  // cout << val << " "
+  // }
+
+  return 0;
+}
